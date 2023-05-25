07f50e310b43caae050812fba377bd3798cc174e
Addr678 <128295657+Addr678@users.noreply.github.com>
2023-05-25 22:17:38 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
 //your JS code here. If required.
+// JavaScript code (script.js)
+function updateTime() {
+  var timerElement = document.getElementById('timer');
+
+  var date = new Date();
+  var time = date.toLocaleTimeString();
+  var formattedDate = date.toDateString();
+
+  timerElement.textContent = "Time: " + time + " | Date: " + formattedDate;
+}
+
+setInterval(updateTime, 1000); // Update time every second
+
